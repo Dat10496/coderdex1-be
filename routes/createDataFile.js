c68088c42f8c9fa1d2472bddd9c051ef7b3d5a33
Dat10496 <votada1996@gmail.com>
2022-07-20 16:46:49 +0700

    change csv data

--- routes/createDataFile.js
+++ routes/createDataFile.js
@@ -2,33 +2,39 @@ const fs = require("fs");
 const csv = require("csvtojson");
 
 const createData = async () => {
-  let newData = await csv().fromFile("../data.csv");
   let data = JSON.parse(fs.readFileSync("../db.json"));
+
+  let newData = await csv().fromFile("../pokemon.csv");
   newData = Array.from(newData);
   newData = newData.slice(0, 721);
 
-  newData = newData.map((data, index) => {
-    const type1 =
-      data.Type1.toString().charAt(0).toLowerCase() + data.Type1.slice(1);
+  newData = newData.map((pokemon, index) => {
+    const pokemonName =
+      pokemon.name.charAt(0).toLowerCase() + pokemon.name.slice(1);
 
-    if (data.Type2) {
-      const type2 =
-        data.Type2.toString().charAt(0).toLowerCase() + data.Type2.slice(1);
+    if (data.type2) {
       return {
         id: `${index + 1}`,
-        name: data.Name,
-        types: [type1, type2],
+        name: pokemonName,
+        height: `${pokemon.height_m} '`,
+        weight: `${pokemon.weight_kg} .lbs`,
+        abilities: pokemon.abilities,
+        types: [pokemon.type1, pokemon.type2],
         url: `http://localhost:5000/images/${index + 1}.jpg`,
       };
     } else {
       return {
         id: `${index + 1}`,
-        name: data.Name,
-        types: [type1],
+        name: pokemonName,
+        height: `${pokemon.height_m} '`,
+        weight: `${pokemon.weight_kg} .lbs`,
+        abilities: pokemon.abilities,
+        types: [pokemon.type1],
         url: `http://localhost:5000/images/${index + 1}.jpg`,
       };
     }
   });
+
   data.totalPokemons = newData.length;
 
   data.data = newData;
